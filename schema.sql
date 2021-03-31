ce431fad1ccc5265f17bf13f5696450c4d285171
code_red <72528267+Corters22@users.noreply.github.com>
2021-03-30 20:09:38 -0500

    added new schema

--- schema.sql
+++ schema.sql
@@ -3,30 +3,24 @@ DROP TABLE if exists actors;
 DROP TABLE if exists principal;
 
 CREATE TABLE "movies" (
-    "movie_id" varchar(30)   NOT NULL,
+    "imdb_title_id" varchar(30)   NOT NULL,
     "title" varchar(500)   NOT NULL,
     "year" int   NOT NULL,
     "genre" varchar(100),
-    "country" varchar(500),
-    "director" varchar(500),
-    "writer" varchar(500),
     CONSTRAINT "pk_movies" PRIMARY KEY (
-        "movie_id"
+        "imdb_title_id"
      )
 );
 
 CREATE TABLE "actors" (
-    "cast_member_id" varchar(30)   NOT NULL,
+    "imdb_name_id" varchar(30)   NOT NULL,
     "name" varchar(100)   NOT NULL,
-    "birth_name" varchar(100),
-    "height" int,
     "place_of_birth" varchar(200),
     "spouses" varchar(200),
+	"children" int   NOT NULL,
     "divorces" int   NOT NULL,
-    "spouses_with_children" int   NOT NULL,
-    "children" int   NOT NULL,
-    CONSTRAINT "pk_actors" PRIMARY KEY (
-        "cast_member_id"
+	CONSTRAINT "pk_actors" PRIMARY KEY (
+        "imdb_name_id"
      )
 );
 
@@ -40,3 +34,6 @@ CREATE TABLE "principal" (
      )
 );
 
+create view full_view as 
+select * from movies as m, actors as a, principal as p
+where m.imdb_title_id = p.movie_id and a.imdb_name_id = p.cast_member_id
